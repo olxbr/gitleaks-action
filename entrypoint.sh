c8f796d2cf28bd9cdecc644ff4345873dedf4607
Juliano Borba <juliano.sato@protonmail.com>
2021-11-22 16:31:18 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,33 +10,37 @@ else
   echo -e "\nConfig not found in $INPUT_CONFIG_PATH... using default"  
 fi
 
-GITLEAKS_VERSION=$(curl -s https://api.github.com/repos/zricethezav/gitleaks/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")') && wget -q https://github.com/zricethezav/gitleaks/releases/download/$GITLEAKS_VERSION/gitleaks-linux-amd64
-mv gitleaks-linux-amd64 gitleaks
-chmod +x gitleaks
+UUID=$(uuidgen)
+REPORT="${UUID}-gitleaks.json"
 
-echo -e "\nrunning gitleaks $(./gitleaks --version) ...\n"
+if [ ! -f "gitleaks-linux-amd64" ]; then
+  GITLEAKS_VERSION=$(curl -s https://api.github.com/repos/zricethezav/gitleaks/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")') && wget -q https://github.com/zricethezav/gitleaks/releases/download/$GITLEAKS_VERSION/gitleaks-linux-amd64
+  chmod +x gitleaks-linux-amd64
+fi
 
-echo '[]' > gitleaks.json
+echo -e "\nrunning gitleaks $(./gitleaks-linux-amd64 --version) ...\n"
+
+echo "[]" > "$REPORT"
 EXIT_CODE=3
 
 if [ "$GITHUB_EVENT_NAME" = "push" -o "$GITHUB_EVENT_NAME" = "workflow_dispatch" ]; then
-  GITLEAKS=$(./gitleaks --path=$GITHUB_WORKSPACE --report="gitleaks.json" --format=JSON --leaks-exit-code=2 --quiet --redact $CONFIG)
+  GITLEAKS=$(./gitleaks-linux-amd64 --path=$GITHUB_WORKSPACE --report="$REPORT" --format=JSON --leaks-exit-code=2 --quiet --redact $CONFIG)
   EXIT_CODE=$?
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF... >commit_list.txt
-  GITLEAKS=$(./gitleaks --path=$GITHUB_WORKSPACE --report="gitleaks.json" --format=JSON --leaks-exit-code=2 --quiet --redact --commits-file=commit_list.txt $CONFIG)
+  GITLEAKS=$(./gitleaks-linux-amd64 --path=$GITHUB_WORKSPACE --report="$REPORT" --format=JSON --leaks-exit-code=2 --quiet --redact --commits-file=commit_list.txt $CONFIG)
   EXIT_CODE=$?
 fi
 
 case $EXIT_CODE in
 0) echo -e "Your code is good to go!\n\n" ;;
 1) echo -e "Gitleaks execution fail!\n\n" ;;
-2) echo -e "Gitleaks encountered leaks!\n\n$(cat gitleaks.json)\n\n" ;;
+2) echo -e "Gitleaks encountered leaks!\n\n$(cat $REPORT)\n\n" ;;
 3) echo -e "Gitleaks not executed!\n\n" ;;
 *) echo -e "Exit code was $EXIT_CODE\n\n" ;;
 esac
 
 echo "::set-output name=exit_code::$EXIT_CODE"
-echo "::set-output name=result::$(jq -c . < gitleaks.json | sed 's/\x27/`/g')"
+echo "::set-output name=result::$(jq -c . < $REPORT | sed 's/\x27/`/g')"
 
-rm -rf gitleaks gitleaks.json
+rm -rf $REPORT
