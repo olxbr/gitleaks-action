1683cc29b3c1a6630e02ed3307a304be9d3a2289
Juliano Borba <juliano.sato@protonmail.com>
2022-01-18 16:33:12 -0300

    Add files via upload (#23)
    * Add files via upload
    
    Uses https://github.com/zricethezav/gitleaks/releases/tag/v7.5.0
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,21 +19,21 @@ echo '[]' > gitleaks.json
 EXIT_CODE=3
 
 if [ "$GITHUB_EVENT_NAME" = "push" -o "$GITHUB_EVENT_NAME" = "workflow_dispatch" ]; then
-  GITLEAKS=$($GITLEAKS_BIN --path=$GITHUB_WORKSPACE --report="gitleaks.json" --format=JSON --leaks-exit-code=2 --quiet --redact $CONFIG)
+  GITLEAKS=$($GITLEAKS_BIN --path=$GITHUB_WORKSPACE -v --report="gitleaks.json" --format=json --leaks-exit-code=2 --redact $CONFIG)
   EXIT_CODE=$?
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
   echo -e "Gitleaks will analyze the following commits..."
   git --git-dir="$GITHUB_WORKSPACE/.git" log --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF.. >commit_list.txt
   cat commit_list.txt
   echo -e "\n...from now!\n"
-  GITLEAKS=$($GITLEAKS_BIN --path=$GITHUB_WORKSPACE --report="gitleaks.json" --format=JSON --leaks-exit-code=2 --quiet --redact --commits-file=commit_list.txt $CONFIG)
+  GITLEAKS=$($GITLEAKS_BIN --path=$GITHUB_WORKSPACE -v --report="gitleaks.json" --format=json --leaks-exit-code=2 --redact --commits-file=commit_list.txt $CONFIG)
   EXIT_CODE=$?
 fi
 
 case $EXIT_CODE in
 0) echo -e "Your code is good to go!\n\n" ;;
 1) echo -e "Gitleaks execution fail!\n\n" ;;
-2) echo -e "Gitleaks encountered leaks!\n\n$(cat gitleaks.json)\n\n" ;;
+2) echo -e "Gitleaks encountered leaks!\n\n$(cat gitleaks.json | jq 'map({lineNumber, commit, rule, file, date, tags})')\n\n" ;;
 3) echo -e "Gitleaks not executed!\n\n" ;;
 *) echo -e "Exit code was $EXIT_CODE\n\n" ;;
 esac
